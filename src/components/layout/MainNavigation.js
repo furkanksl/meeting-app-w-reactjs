a64738e7661c61edc6f92157709f639a3857825c
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-17 16:40:08 +0300

    Show badge on favorites title on navbar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./MainNavigation.module.css";
 
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
+
 function MainNavigation() {
+  const favoritesContext = useContext(FavoritesContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>Meetups</div>
@@ -14,7 +19,12 @@ function MainNavigation() {
             <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={styles.badge}>
+                {favoritesContext.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoritesContext = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesContext.favorites.length === 0) {
+    content = <p>You have no favorite meetups.</p>;
+  } else {
+    content = <MeetupList meetups={favoritesContext.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>Favorites Page</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
