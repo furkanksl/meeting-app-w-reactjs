9f10f18b617f0cfad2f8e750f434b4d7c621b554
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-17 16:02:29 +0300

    Update base constants file

--- src/base/Base.js
+++ src/base/Base.js
@@ -1,3 +1,4 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const METHOD_POST = "POST";
+export const MEETUPS_URL = BASE_URL + "meetups.json";
 export const CONTENT_TYPE_JSON = { "Content-Type": "application/json" };
